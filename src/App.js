0ec65503a11f1d4e2d037f81a0d44910d4c4f719
daeun <sung980524@naver.com>
2021-08-16 13:41:53 +0900

    신발샵 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import './App.css';
+import {Navbar,Container,Nav,NavDropdown,Button} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">신발샵</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="background">
+        <h1>20% Season OFF</h1>
+        <p>
+          This site offer a biggest sale product in the Korea.
+          We hope you to enjoy here
+        </p>
+        <p>
+        <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt=""></img>
+            <h4>상품명</h4>
+            <p>상품설명&가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" alt=""></img>
+            <h4>상품명</h4>
+            <p>상품설명&가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" alt=""></img>
+            <h4>상품명</h4>
+            <p>상품설명&가격</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
